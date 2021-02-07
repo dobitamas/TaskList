af5159140acb3b62dc187aa4a8cc703fbfb2274d
Dobi Tamas <dobi.tamas0428@gmail.com>
2021-02-07 14:01:29 +0100

    Added SQL keywords and IsDeleted field

--- TaskList.Domain/BaseEntity.cs
+++ TaskList.Domain/BaseEntity.cs
@@ -9,12 +9,21 @@ namespace TaskList.Domain
         [Key]
         public int Id { get; set; }
 
+        [ScaffoldColumn(false)]
         public DateTime DateCreated { get; set; }
 
+        [ScaffoldColumn(false)]
         public DateTime LastModified { get; set; }
 
+        [ScaffoldColumn(false)]
+        [StringLength(255)]
         public string CreatedById { get; set; }
 
+        [ScaffoldColumn(false)]
+        [StringLength(255)]
         public string ModifiedById { get; set; }
+
+        [ScaffoldColumn(false)]
+        public bool IsDeleted { get; set; } = false;
     }
 }
